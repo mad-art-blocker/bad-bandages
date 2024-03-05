a168803f4b10839513d04bf4f4ae33f525740de5
Bert G. <madartblocker@gmail.com>
2024-03-05 12:29:58 -0800

    Added site logo script to global js

--- global-scripts.js
+++ global-scripts.js
@@ -1,22 +1,22 @@
-
 //
-// import "/scripts/bandages.js";
+//  ---
+
+import siteLogo from '/bad_bandage.png'
+
+//  ---
+//
 document.addEventListener('DOMContentLoaded', () => {
 
-  // Proops Object
   // WEBSITE TITLES/INTERFACE PROPERTIES:
   const bandagesKeys = {
-    webSiteTitle: 'BAD\'BANDAGES',
+    webSiteTitle: 'BAD.BANDAGES',
     subTitle: 'Malas Vendas',
     serveYear: new Date(),
     'role Token': 'Beto Vector'
   };
 
-  // console.log(typeof bandagesKeyse)
 
-
-
-  const injectTitle = document.querySelectorAll('.siteTitle');
+  const injectTitle = document.querySelectorAll('.siteTitleJS');
   for (let it = 0; it < injectTitle.length; it++) {
     injectTitle[it].innerText = `${bandagesKeys.webSiteTitle}`;
   }
@@ -24,18 +24,27 @@ document.addEventListener('DOMContentLoaded', () => {
   // Author, Title,  or Token:
   document.querySelector('.authorToken').innerText = bandagesKeys['role Token'];
 
-  // Special Page:
+  // Sub-title or Token:
   document.querySelector('.subTitles').innerText = bandagesKeys.subTitle;
+
+  // Site Logo:
+  // <h1 class="util-text-family">${bandagesKeys.webSiteTitle}</h1>
+  document.querySelector('.siteLogoJS').innerHTML = `
+<a href="/">
+  <img class="main-logo" src="${siteLogo}" alt="Image logo decoration of a bandage" />
+</a>
+`
+
+  // MOBILE MENU
+  const hamburgerButton = document.querySelector('.hamburger-icon');
+  const menuMobile = document.querySelector('.util-mobile-menu');
+
+  hamburgerButton.addEventListener('click', () => menuMobile.classList.toggle('open'));
+
 
   // Footer Date:
   // const serveYear = new Date();
   document.querySelector('.thisYear').innerText = bandagesKeys.serveYear.getFullYear();
 
 
-
-  // MOBILE MENU
-  const hamburgerButton = document.querySelector('.hamburger-icon');
-  const menuMobile = document.querySelector('.util-mobile-menu');
-
-  hamburgerButton.addEventListener('click', () => menuMobile.classList.toggle('open'))
 });
